72bbf166f35ad3e3578ee0888e583ac965411c8d
Akanksha Lavale <akankshalavale06@gmail.com>
2025-07-23 08:43:41 +0530

    Time: 8 ms (50.86%), Space: 7.8 MB (64.03%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,19 +1,10 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0){
-            return 0;
-        }else if(n==1){
-            return 1 ;
+        if(n<=1){
+            return n;
         }
-        int firstterm =0;
-        int secterm =1;
-        for(int i=1;i<=n;i++){
 
-            int thirdterm=firstterm+secterm;
-            firstterm=secterm;
-            secterm=thirdterm;
-        }
-        return firstterm;
+        return fib(n-1)+fib(n-2);
     }
 };
