25e7df36cc8b0eaab1ac3dfa4ef03ce0b3ba90f1
Akanksha Lavale <akankshalavale06@gmail.com>
2025-08-01 09:25:34 +0530

    Time: 239 ms (54.15%), Space: 146.8 MB (42.38%) - LeetHub

---
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    void merge(vector<int>& nums, int start, int mid, int end) {
+        vector<int> temp(end - start + 1);
+
+        int left = start, right = mid + 1, index = 0;
+
+        while (left <= mid && right <= end) {
+            if (nums[left] <= nums[right]) {
+                temp[index] = nums[left];
+                index++;
+                left++;
+            } else {
+                temp[index] = nums[right];
+                index++;
+                right++;
+            }
+        }
+
+        while (left <= mid) {
+            temp[index] = nums[left];
+            index++;
+            left++;
+        }
+
+        while (right <= end) {
+            temp[index] = nums[right];
+            index++;
+            right++;
+        }
+
+        for (int i = 0; i < temp.size(); i++) {
+            nums[start + i] = temp[i];
+        }
+    }
+
+    void mergesort(vector<int>& nums, int start, int end) {
+        if (start >= end) return;
+
+        int mid = start + (end - start) / 2;
+
+        mergesort(nums, start, mid);
+        mergesort(nums, mid + 1, end);
+        merge(nums, start, mid, end);
+    }
+
+    vector<int> sortArray(vector<int>& nums) {
+        mergesort(nums, 0, nums.size() - 1);
+        return nums;
+    }
+};
