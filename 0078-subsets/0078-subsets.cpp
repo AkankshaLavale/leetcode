81446ecbf9a9920e6f8a30e00a918ba61d163506
Akanksha Lavale <akankshalavale06@gmail.com>
2025-08-04 09:39:20 +0530

    Time: 2 ms (41.05%), Space: 12.7 MB (27.97%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 
-    void subsequence(vector<int>& arr, int index, int n, vector<vector<int>>& ans, vector<int> temp) {
+    void subsequence(vector<int>& arr, int index, int n, vector<vector<int>>& ans, vector<int>& temp) {
         if (index == n) {
             ans.push_back(temp);
             return;
@@ -11,6 +11,7 @@ public:
         subsequence(arr, index + 1, n, ans, temp);
         temp.push_back(arr[index]);
         subsequence(arr, index + 1, n, ans, temp);
+        temp.pop_back();
     }
 
     vector<vector<int>> subsets(vector<int>& nums) {
