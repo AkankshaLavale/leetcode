1d5673c05f066f4ea130e5f21306e795597db2ee
Akanksha Lavale <akankshalavale06@gmail.com>
2025-08-11 00:10:13 +0530

    Time: 2 ms (34.81%), Space: 9.2 MB (41.89%) - LeetHub

--- 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
@@ -7,7 +7,7 @@ public:
         string s = to_string(n);
         sort(s.begin(), s.end()); 
 
-        for (int i = 0; i < 32; i++) {
+        for (int i = 0; i < 31; i++) {
             long long power = pow(2, i);
             string p = to_string(power);
             sort(p.begin(), p.end());
