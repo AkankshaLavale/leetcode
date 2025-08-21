64eaec518dd6aa24323e48c33c565c61c7958483
Akanksha Lavale <akankshalavale06@gmail.com>
2025-08-21 14:45:26 +0530

    Time: 1446 ms (5.02%), Space: 25.2 MB (71.22%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -5,7 +5,7 @@ public:
         int duplicate =0;
         int missing =0;
 
-        for(int i=0;i<=n;i++){
+        for(int i=1;i<=n;i++){
             int count=0;
 
             for(int j=0;j<n;j++){
