0706d183708a7c9cb5a034cd8b48ba97076c0190
Akanksha Lavale <akankshalavale06@gmail.com>
2025-08-27 22:55:27 +0530

    Time: 1585 ms (6.21%), Space: 74.2 MB (92.66%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int i = 0;
+        int n = nums.size();
+
+        while (i < n) {
+            int j = i + 1;
+            while (j <= i + k && j < n) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+                j++;
+            }
+            i++;
+        }
+
+        return false;
+    }
+};
