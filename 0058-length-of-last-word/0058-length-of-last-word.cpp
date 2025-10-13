ed923d80d46a7dd50696aa069fe8f9b35e8699aa
Akanksha Lavale <akankshalavale06@gmail.com>
2025-10-13 20:32:21 +0530

    Time: 0 ms (100%), Space: 9 MB (33.06%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-    int count = 0;
-    for (int i = s.length() - 1; i >= 0; i--) {
-        if (s[i] != ' ') {
-            count++;        
-        }else if(count>0){
-            break;
+        int count = 0;
+        
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s[i] == ' ' && count > 0) {
+
+                break;
+            } else if (s[i] != ' ') {
+                count++;
+            }
         }
+        return count;
     }
-
-    return count;
-}
-
 };
-
-
-
-
