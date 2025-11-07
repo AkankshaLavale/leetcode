c2fc8a3ddd6be3ce758acd92651861e0ca2c221e
Akanksha Lavale <akankshalavale06@gmail.com>
2025-11-07 23:30:05 +0530

    Time: 0 ms (100%), Space: 9.8 MB (94.01%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -2,17 +2,20 @@ class Solution {
 public:
     bool isPalindrome(string s) {
 
-        string ch="";
-        for(char c:s){
-            if(isalnum(c)){
-                ch+=tolower(c);
-            }
-        }
-        string reversed=ch;
-        reverse(reversed.begin(),reversed.end());
-        return ch==reversed;
+        int left = 0;
+        int right = s.length()-1;
 
-          
-        
+        while(left<right){
+
+            while(left<right && !isalnum(s[left])) left++;
+            while(left<right && !isalnum(s[right])) right--;
+
+            if(tolower(s[left])!=tolower(s[right])){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 };
