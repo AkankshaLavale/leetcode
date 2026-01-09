595ebd6c92775546a0245b876ca80be2fd019e85
Akanksha Lavale <akankshalavale06@gmail.com>
2026-01-10 02:20:47 +0530

    Time: 0 ms (100%), Space: 63.6 MB (10.3%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -3,21 +3,21 @@ public:
     int peakIndexInMountainArray(vector<int>& arr) {
         int start = 0;
         int end = arr.size()-1;
-        int ans = -1;
+        
 
 
-        while(start<=end){
+        while(start<end){
             int mid = start+(end-start)/2;
 
             if(arr[mid]<arr[mid+1]){
                start = mid+1;
             }else{
-               ans = mid;
-               end = mid-1;
+              end = mid;
+              
             }
 
         }
 
-        return ans;
+        return start;
     }
 };
