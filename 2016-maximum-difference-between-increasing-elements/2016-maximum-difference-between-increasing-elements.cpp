2fe886aa64f300268904971dfc3a39d255543f4b
Akanksha Lavale <akankshalavale06@gmail.com>
2026-01-22 15:47:34 +0530

    Time: 0 ms (100%), Space: 12 MB (30.53%) - LeetHub

--- 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.cpp
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.cpp
@@ -3,19 +3,15 @@ public:
     int maximumDifference(vector<int>& nums) {
         int n = nums.size();
         int ans =-1;
-        vector<int>sufixMax(n);
-        sufixMax[n-1] = nums[n-1];
-
-        for(int i=n-2;i>-1;i--){
-            sufixMax[i] = max(nums[i],sufixMax[i+1]);
-        }
-
-        for(int i=0;i<n-1;i++){
-            if(sufixMax[i+1]>nums[i]){
-               ans=max(ans,sufixMax[i+1]-nums[i]);
+        int min = nums[0];
+        
+        for(int i=1;i<n;i++){
+           if (nums[i] > min) {
+            ans = max(ans, nums[i] - min);
+           } else {
+            min = nums[i];
             }
         }
-
         return ans;
     }
 };
