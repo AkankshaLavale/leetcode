88ffd3b004a82ac7de646f0e318154a13ce78111
Akanksha Lavale <akankshalavale06@gmail.com>
2026-01-27 21:20:43 +0530

    Time: 0 ms (100%), Space: 9.1 MB (57.56%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        int i = (int)num1.size() - 1;
-        int j = (int)num2.size() - 1;
-        int carry = 0;
-        string result;
-        result.reserve(max(num1.size(), num2.size()) + 1); 
 
-        while (i >= 0 || j >= 0 || carry) {
-            int d1 = (i >= 0) ? num1[i] - '0' : 0;
-            int d2 = (j >= 0) ? num2[j] - '0' : 0;
-            int sum = d1 + d2 + carry;
+        int firstend = num1.size() - 1;
+        int secondend = num2.size() - 1;
+
+        string ans = "";
+        int carry = 0;
+
+        while (firstend >= 0 || secondend >= 0 || carry != 0) {
+
+            int sum = carry;
+
+            if (firstend >= 0) {
+                sum += num1[firstend] - '0';
+                firstend--;
+            }
+
+            if (secondend >= 0) {
+                sum += num2[secondend] - '0';
+                secondend--;
+            }
+
+            ans.push_back((sum % 10) + '0');
             carry = sum / 10;
-            result.push_back(char('0' + (sum % 10)));
-            --i; --j;
         }
 
-        reverse(result.begin(), result.end());
-        return result;
+        reverse(ans.begin(), ans.end());
+
+        return ans;
     }
 };
