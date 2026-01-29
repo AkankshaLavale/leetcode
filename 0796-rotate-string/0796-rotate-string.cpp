b1675c24f0d73a07b4a55d8b26abccfa32813451
Akanksha Lavale <akankshalavale06@gmail.com>
2026-01-29 23:23:06 +0530

    Time: 0 ms (100%), Space: 8.1 MB (84.17%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+
+        if (s.size() != goal.size()) {
+            return false;
+        }
+
+        string combined = s + s;
+
+        if (combined.find(goal) != string::npos) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
