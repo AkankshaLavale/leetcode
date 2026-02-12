07e5b79662c841973e478f9df2ac3b7cd56fbee6
Akanksha Lavale <akankshalavale06@gmail.com>
2026-02-13 00:58:06 +0530

    Time: 1045 ms (42.47%), Space: 7.8 MB (20.41%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -3,6 +3,7 @@ public:
     bool checkPerfectNumber(int num) {
 
         int sum =0;
+        if(num<=1) return false;
         for(int i=1;i<=num/2;i++){
            if(num%i==0){
             sum+=i;
