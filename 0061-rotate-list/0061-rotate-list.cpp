9dcb75650f7af62380acc94b04c674488aac554a
Akanksha Lavale <akankshalavale06@gmail.com>
2026-02-18 23:37:54 +0530

    Time: 0 ms (100%), Space: 16.3 MB (94.17%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -11,6 +11,30 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
+        if (!head || !head->next || k == 0) return head;
+
         
+        int count = 1;
+        ListNode* tail = head;
+        while (tail->next) {
+            tail = tail->next;
+            count++;
+        }
+
+        tail->next = head;
+
+        
+        k = k % count;
+        int stepsToNewTail = count  - k;
+        ListNode* newTail = head;
+        for (int i = 1; i < stepsToNewTail; i++) {
+            newTail = newTail->next;
+        }
+
+        
+        ListNode* newHead = newTail->next;
+        newTail->next = nullptr;
+
+        return newHead;
     }
 };
