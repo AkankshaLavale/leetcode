31e737c3ec1be6331a5b3b0806081657a13b9bf3
Akanksha Lavale <akankshalavale06@gmail.com>
2026-03-25 09:24:34 +0530

    Time: 0 ms (100%), Space: 9.4 MB (77.89%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,61 +1,32 @@
 class MyStack {
-    queue<int>q1;
-    queue<int>q2;
 public:
+    queue<int>q;
     MyStack() {
         
     }
-    bool empty() {
-        return q1.empty()&&q2.empty();
-    }
     
     void push(int x) {
-
-        if(empty()){
-            q1.push(x);
-        }else if(q1.empty()){
-            q2.push(x);
-        }else{
-            q1.push(x);
+        q.push(x);
+         int n = q.size();
+        for(int i=0;i<n-1;i++){
+            q.push(q.front());
+            q.pop();
         }
-        
     }
     
     int pop() {
-       if(empty()) return 0;
-       else if(q1.empty()){
-        while(q2.size()>1){
-            q1.push(q2.front());
-            q2.pop();
-        }
-        int element = q2.front();
-        q2.pop();
-        return element;
-       }else{
-        while(q1.size()>1){
-            q2.push(q1.front());
-            q1.pop();
-        }
-        int element = q1.front();
-        q1.pop();
-        return element;
-       }
+        int val = q.front();
+        q.pop();
+        return val;
     }
     
     int top() {
-        if(empty()) return 0;
-         
-        else if(q1.empty()){
-            return q2.back();
-        }else{
-            return q1.back();
-        }
-
-
-        
+        return q.front();
     }
     
-    
+    bool empty() {
+       return q.empty();
+    }
 };
 
 /**
